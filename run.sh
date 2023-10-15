34a0d4fe315f6f3ab54bedd22ef7e04e268f1e30
syntaspeech <syntaspeech@gmail.com>
2023-10-15 23:45:28 +0800

    update 1015

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # ssh-keygen -t rsa -f ~/.ssh/zera_id_rsa
 eval $(ssh-agent)
-ssh-add ~/.ssh/zera_id_rsa
+ssh-add ~/.ssh/genefaceplusplus_id_rsa
 
-git remote add origin git@github.com:real3dportrait/real3dportrait.github.io.git
+git remote add origin git@github.com:genefacePlusPlus/genefacePlusPlus.github.io.git
 git branch -M main
 git push -u origin main
