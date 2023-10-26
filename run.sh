b4b69fe0030bc99ab646a5f6694ab658b9969eea
genefaceplusplus <geneface.lrs3.1@gmail.com>
2023-10-26 23:47:15 +0800

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 # ssh-keygen -t rsa -f ~/.ssh/zera_id_rsa
 eval $(ssh-agent)
 ssh-add ~/.ssh/genefaceplusplus_id_rsa
-
+git config user.name "genefaceplusplus"
+git config user.email "geneface.lrs3.1@gmail.com"
 git remote add origin git@github.com:genefacePlusPlus/genefacePlusPlus.github.io.git
-git branch -M master
-git push -u origin master
+git branch -M main
+git push -u origin main
